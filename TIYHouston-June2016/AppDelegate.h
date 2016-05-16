7d7f5603046fdc66a7beb413d8641901fa5a67dd
azamsharp <azamsharp@gmail.com>
2016-05-16 13:40:04 -0500

    Initial Commit

---
+++ TIYHouston-June2016/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TIYHouston-June2016
+//
+//  Created by Mohammad Azam on 5/16/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TIYHouston-June2016/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TIYHouston-June2016
+//
+//  Created by Mohammad Azam on 5/16/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
